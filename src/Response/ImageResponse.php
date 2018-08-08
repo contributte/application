6028a2882c6224d2cee2133171ec968e9baa42cf
freiondrej <37588173+freiondrej@users.noreply.github.com>
2018-08-08 10:39:03 +0200

    Allow sending the image as png with configurable quality (#16)

--- src/Response/ImageResponse.php
+++ src/Response/ImageResponse.php
@@ -17,16 +17,26 @@ class ImageResponse implements IResponse
 	/** @var Image|string */
 	private $image;
 
+	/** @var int */
+	private $type;
+
+	/** @var int|null */
+	private $quality;
+
 	/**
 	 * @param Image|string $image
+	 * @param int $type
+	 * @param int $quality
 	 */
-	public function __construct($image)
+	public function __construct($image, $type = Image::JPEG, $quality = null)
 	{
 		if (!$image instanceof Image && !file_exists($image)) {
 			throw new InvalidArgumentException('Image must be Nette\Utils\Image or file path');
 		}
 
 		$this->image = $image;
+		$this->type = $type;
+		$this->quality = $quality;
 	}
 
 	public function send(IHttpRequest $httpRequest, IHttpResponse $httpResponse): void
@@ -37,7 +47,7 @@ class ImageResponse implements IResponse
 			$image = Image::fromFile($this->image);
 		}
 
-		$image->send();
+		$image->send($this->type, $this->quality);
 	}
 
 }
